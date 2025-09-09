649fcdc68d661fa66f8d0e0cd7c46a135bbf1981
Isaac Powers <isaac_powers@MacBook-Air-2.local>
2025-09-08 21:00:49 -0700

    index fixed?

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -1,6 +1,6 @@
 extends Node
 
-@onready var online := false
+@onready var online := true
 
 @onready var main_menu = $CanvasLayer/MainMenu
 @onready var address_entry = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/AddressEntry
